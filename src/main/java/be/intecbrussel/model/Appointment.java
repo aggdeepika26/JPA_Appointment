66f40fcbaab5856544d4597fcea9644caebb9846
Deepika <adeepika26@yahoo.in>
2024-04-30 15:53:59 +0200

    appointment

---
+++ src/main/java/be/intecbrussel/model/Appointment.java
@@ -0,0 +1,101 @@
+package be.intecbrussel.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "Appointment_Info")
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "Appointment_Id")
+    private long id;
+    private String title;
+    private String description;
+    private LocalDate appointmentDate;
+    private LocalTime appointmentTime;
+    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.PERSIST})
+    private User user;
+
+    public Appointment()
+    {
+
+    }
+
+    public Appointment(long id, String title, String description, LocalDate appointmentDate, LocalTime appointmentTime, User user) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime;
+        this.user = user;
+    }
+
+    public Appointment(String title, String description, LocalDate appointmentDate, LocalTime appointmentTime) {
+        this.title = title;
+        this.description = description;
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime;
+    }
+
+    public Appointment(long id, LocalDate appointmentDate, LocalTime appointmentTime) {
+        this.id = id;
+        this.appointmentDate = appointmentDate;
+        this.appointmentTime = appointmentTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDate appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public LocalTime getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(LocalTime appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", appointmentDate=" + appointmentDate +
+                ", appointmentTime=" + appointmentTime +
+                ", user=" + user +
+                '}';
+    }
+}
+
---
+++ src/main/java/be/intecbrussel/model/User.java
@@ -0,0 +1,56 @@
+package be.intecbrussel.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @Column(name = "User_Id")
+    private int userId;
+    private String firstName;
+    private String lastName;
+
+    public User() {
+    }
+
+    public User(int userId, String firstName, String lastName) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
